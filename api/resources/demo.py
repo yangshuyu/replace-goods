74cb1c7febe5f8bd6713329c36c030d340a0d2ca
yangshuyu <810043299@qq.com>
2019-01-14 17:03:53 +0800

    yangshuyu

--- api/resources/demo.py
+++ api/resources/demo.py
@@ -3,7 +3,7 @@ from flask_restful import Resource
 
 class DemoResource(Resource):
     def get(self):
-        return {'data': 'jenkies-yangshuyu-yangyangyang'}
+        return {'data': 'jenkies-yangshuyu-shushushu'}
 
     def post(self):
         self.get()
